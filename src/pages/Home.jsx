998bbcb73f5a8defbc3bc75b34e575a82e9b206b
Evaristo GC <evaristogaell@gmail.com>
2024-09-11 19:47:19 +0200

    returning search results

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
-
 import MovieCard from "../components/MovieCard"
+
+import './MoviesGrid.css'
 
 const moviesURL = import.meta.env.VITE_API
 const apikey = import.meta.env.VITE_API_KEY
@@ -23,6 +24,7 @@ const Home = () => {
     return (
         <div className="container">
             <h2 className="title">
+                Melhores filmes:
                 <div className="movies-container">
                     {topMovies.length === 0 && <p>Carregando...</p>}
                     {topMovies.length > 0 && topMovies.map((movie) => <MovieCard movie={movie} key={movie.id} />)}

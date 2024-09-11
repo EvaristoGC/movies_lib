61dea8af7b416c3c8f270d3a63594965018723b6
Evaristo GC <evaristogaell@gmail.com>
2024-09-11 17:29:05 +0200

    showing images, titles and average votes of the films

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,33 @@
-const Home = () =>{
+import { useState, useEffect } from "react"
+
+import MovieCard from "../components/MovieCard"
+
+const moviesURL = import.meta.env.VITE_API
+const apikey = import.meta.env.VITE_API_KEY
+
+const Home = () => {
+    const [topMovies, setTopMovies] = useState([])
+
+    const getTopRateMovies = async (url) => {
+        const res = await fetch(url)
+        const data = await res.json()
+
+        setTopMovies(data.results)
+    }
+
+    useEffect(() => {
+        const topRatedUrl = `${moviesURL}top_rated?${apikey}`
+        getTopRateMovies(topRatedUrl)
+    }, [])
+
     return (
-        <div>
-            Home
+        <div className="container">
+            <h2 className="title">
+                <div className="movies-container">
+                    {topMovies.length === 0 && <p>Carregando...</p>}
+                    {topMovies.length > 0 && topMovies.map((movie) => <MovieCard movie={movie} key={movie.id} />)}
+                </div>
+            </h2>
         </div>
     )
 }

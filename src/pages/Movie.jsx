89fe70bb4a2bb5b0eaa0e1093cdaa152065c3a26
Evaristo GC <evaristogaell@gmail.com>
2024-09-12 19:16:39 +0200

    provisory commit

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,4 +1,20 @@
-const Movie = () =>{
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import {
+    BsGraphUp,
+    BsWallet2,
+    BsHourglassSplit,
+    BsFillfileEarmarkTextFill
+} from 'react-icons/bs'
+import MovieCard from "../components/MovieCard"
+
+import './Movie.css'
+
+const moviesURL = import.meta.env.VITE_API
+const apikey = import.meta.env.VITE_API_KEY
+
+const Movie = () => {
+
     return (
         <div>
             Movie
